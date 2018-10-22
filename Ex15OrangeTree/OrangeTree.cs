c2a302fbe244b9f8f58e074b65a46be79d7a8d91
Daniel Helmer Sørensen <dani925y@edu.eal.dk>
2018-10-22 12:25:22 +0200

    Add project files.

---
+++ Ex15OrangeTree/OrangeTree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex15OrangeTree
+{
+    public class OrangeTree
+    {
+        private int age;
+        private int height;
+
+
+        public OrangeTree(int age, int height)
+        {
+            this.age = age;
+            this.height = height;
+        }
+
+
+        public int Age
+        {
+            get;
+            private set;
+
+        }
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                height = value;
+            }
+        }
+        public bool TreeAlive
+        {
+            get;
+            set;
+        }
+        public int NumOranges
+        {
+            get;
+            private set;
+        }
+        public int OrangesEaten
+        {
+            get;
+            private set;
+        }
+
+        public void OneYearPasses()
+        {
+            Age++;
+
+            if (Age < 80)
+            {
+                Height += 2;
+            }
+
+            OrangesEaten = 0;
+            
+
+            if (Age > 1 && Age <= 80)
+            {
+                NumOranges += 5;
+            }
+
+
+            if (Age > 80)
+            {
+                TreeAlive = false;
+                NumOranges = 0;
+            }
+
+        }
+
+
+        public void EatOrange(int v)
+        {
+            OrangesEaten += v;
+
+            if (Age < 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Ex15OrangeTreeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ex15OrangeTreeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ex15OrangeTreeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c44ca09-4c1a-442f-88da-e53f980e2cce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
